4efb046ff6359b41ccd86b4dde5b53380642acf5
Lmc95 <95carranzalm@gmail.com>
2023-11-30 18:15:30 -0300

    paginas creadas (ver_todo, reproduccion)

--- js/app.js
+++ js/app.js
@@ -1,33 +1,43 @@
 import { peliculas, series } from "./movies.js";
 
+// MENU MOBILE - var
 const menuMobile = document.querySelector('.menu_mobile')
 const btnMenu = document.querySelector('.menu_btn');
-
 let contadorMenu = 0;
+
+// CARRUSEL PELICULAS Y SERIES - var
+const carrusel = document.querySelector('.carrusel');
+const btnIzq = document.querySelector('.btn_izq');
+const btnDer = document.querySelector('.btn_der');
+
+
+//EVENTOS --
+// Mostrar menu
 btnMenu.addEventListener('click', () => {
     contadorMenu++;
     animacionMenu();
 })
 
-
-// Abrir y cerrar menu
-const animacionMenu = () => {
-    if(contadorMenu % 2 === 0){
-        // Cerrar menu
-        menuMobile.classList.remove('abrir_menu')
-    }else {
-        // Abrir menu
-        menuMobile.classList.add('abrir_menu')
-    }
-}
-
-const carrusel = document.querySelector('.carrusel');
-const btnIzq = document.querySelector('.btn_izq');
-const btnDer = document.querySelector('.btn_der');
-
+// Scroll de carrusel con botones
 btnIzq.addEventListener('click', () => {
     carrusel.scrollLeft -= carrusel.clientWidth * 1;
 })
 btnDer.addEventListener('click', () => {
     carrusel.scrollLeft += carrusel.clientWidth * 1;
 })
+
+// FUNCIONES --
+
+// MENU: Abrir y cerrar menu
+const animacionMenu = () => {
+    if (contadorMenu % 2 === 0) {
+        // Cerrar menu
+        menuMobile.classList.remove('abrir_menu')
+    } else {
+        // Abrir menu
+        menuMobile.classList.add('abrir_menu')
+    }
+}
+
+
+
